48e5772d67546fbaf1fe65fc0e016f497a08530b
Lucas Quental <lucas.quental.001@gmail.com>
2022-10-13 00:02:14 -0300

    Update index.tsx

--- pages/About/index.tsx
+++ pages/About/index.tsx
@@ -10,8 +10,8 @@ export default function About(){
                     <div className="row justify-content-center">
                         <div className="col-lg-8 col-xxl-6">
                             <div className="text-center my-5">
-                                <h1 className="fw-bolder mb-3">O Objeto de Aprendizagem / Recurso Educacional Digital</h1>
-                                <p className="lead fw-normal text-muted mb-4">Nesta página vamos falar um pouco sobre o Objeto de Aprendizagem que foi implementado neste site.</p>
+                                <h1 className="fw-bolder mb-3">O Recurso Educacional Digital</h1>
+                                <p className="lead fw-normal text-muted mb-4">Nesta página vamos falar um pouco sobre o Recurso Educacional Digital que foi implementado neste site.</p>
                             </div>
                         </div>
                     </div>
@@ -24,7 +24,7 @@ export default function About(){
                         <div className="col-lg-6"><img className="img-fluid rounded mb-5 mb-lg-0" src="" alt="..." /></div>
                         <div className="col-lg-6">
                             <h2 className="fw-bolder">Objetivo</h2>
-                            <p className="lead fw-normal text-muted mb-0">Este Objeto de Aprendizagem tem como objetivo, ensinar sobre os conceitos da computação em nuvem e seus funcionamentos teóricos e práticos.</p>
+                            <p className="lead fw-normal text-muted mb-0">Este Recurso Educacional Digital tem como objetivo, ensinar sobre os conceitos da computação em nuvem e seus funcionamentos teóricos e práticos.</p>
                         </div>
                     </div>
                 </div>
@@ -37,7 +37,7 @@ export default function About(){
                             <img className="img-fluid rounded mb-5 mb-lg-0" src="" style={{width:'300px'}} alt="..." /></div>
                         <div className="col-lg-6">
                             <h2 className="fw-bolder">Recursos</h2>
-                            <p className="lead fw-normal text-muted mb-0">Neste Objeto de Aprendizagem nós temos vários recursos para auxiliar o usuário a entender melhor o conteúdo como textos explicativos sobre os diferentes tópicos abordados, videos sobre alguns casos de uso da computação em nuvem e testes para avaliar o seu conhecimento sobre o que foi apresentado no site.</p>
+                            <p className="lead fw-normal text-muted mb-0">Neste Recurso Educacional Digital nós temos vários recursos para auxiliar o usuário a entender melhor o conteúdo como textos explicativos sobre os diferentes tópicos abordados, videos sobre alguns casos de uso da computação em nuvem e testes para avaliar o seu conhecimento sobre o que foi apresentado no site.</p>
                         </div>
                     </div>
                 </div>
@@ -49,7 +49,7 @@ export default function About(){
                             <img className="img-fluid rounded mb-5 mb-lg-0" src="" style={{width:'300px'}} alt="..." /></div>
                         <div className="col-lg-6">
                             <h2 className="fw-bolder">Publico Destinado</h2>
-                            <p className="lead fw-normal text-muted mb-0"></p>
+                            <p className="lead fw-normal text-muted mb-0">TEXTO AQUI</p>
                         </div>
                     </div>
                 </div>
@@ -61,6 +61,18 @@ export default function About(){
                             <img className="img-fluid rounded mb-5 mb-lg-0" src="" style={{width:'300px'}} alt="..." /></div>
                         <div className="col-lg-6">
                             <h2 className="fw-bolder">Palavras Chave</h2>
+                            <p className="lead fw-normal text-muted mb-0">TEXTO AQUI</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="py-5 bg-light" id="scroll-target">
+                <div className="container px-5 my-5">
+                    <div className="row gx-5 align-items-center">
+                        <div className="col-lg-6">
+                            <img className="img-fluid rounded mb-5 mb-lg-0" src="" style={{width:'300px'}} alt="..." /></div>
+                        <div className="col-lg-6">
+                            <h2 className="fw-bolder">Uso da Nuvem</h2>
                             <p className="lead fw-normal text-muted mb-0"></p>
                         </div>
                     </div>
