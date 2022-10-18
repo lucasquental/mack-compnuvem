c972f0fad8cf0485309a19b1b11aa72240001d2d
Lucas Quental <lucas.quental.001@gmail.com>
2022-10-18 18:26:43 -0300

    Update index.tsx

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,14 +1,25 @@
 import { DropdownButton } from "../dropdown";
 import { DropdownButtonQuest } from "../dropdownquest";
+import { useState } from "react";
 
 export function Navbar(){
- 
+  const [show,setShow] = useState(false)
+  const [collapse,setCollapse] = useState('')
+
+  function handleToggler(){
+    if(show==false){
+      setCollapse('show')
+    }else{
+      setCollapse('')
+    }
+     setShow(!show)
+  }
     return(
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark align-items-center">
       <div className="container px-5">
           <a className="navbar-brand" href="/">Computação em Nuvem</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <button className="navbar-toggler" onClick={handleToggler} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+          <div className={`collapse navbar-collapse ${collapse}`} id="navbarSupportedContent">
               <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                   <li className="nav-item"><a className="nav-link" href="/">Menu</a></li>
                   <li className="nav-item"><a className="nav-link" href="/About">Sobre</a></li>
