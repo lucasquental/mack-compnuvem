c484b64ea8fc670a8d24c3ed23a20dd5a4bb225b
Lucas Quental <lucas.quental.001@gmail.com>
2022-11-10 20:38:10 -0300

    Update index.tsx

--- pages/referencias/index.tsx
+++ pages/referencias/index.tsx
@@ -18,17 +18,19 @@ export default function About(){
          
             <section className="py-5 bg-light" id="scroll-target">
                 <div className="container px-5 my-5">
+                    <p className="lead fw-normal"><strong><em>Definição de Computação em Nuvem</em>:</strong></p>
+                    <p className="lead fw-normal"><em>GOVERNO DO BRASIL. Características essenciais de computação em nuvem. Disponível em: https://www.gov.br/governodigital/pt-br/estrategias-e-politicas-digitais/computacao-em-nuvem/caracteristicas-essenciais-de-computacao-em-nuvem. Acesso em: 9 nov. 2022.</em></p>
+                    <p className="lead fw-normal"><em>MELL, P. M.; GRANCE, T. The NIST Definition of Cloud Computing. Disponível em: https://www.nist.gov/publications/nist-definition-cloud-computing. Acesso em: 9 nov. 2022.</em><small></small></p>
                     <p className="lead fw-normal"><strong><em>IaaS, PaaS e SaaS</em>:</strong></p>
                     <p className="lead fw-normal"><em>GOVERNO DO BRASIL. Características essenciais de computação em nuvem. Disponível em: https://www.gov.br/governodigital/pt-br/estrategias-e-politicas-digitais/computacao-em-nuvem/caracteristicas-essenciais-de-computacao-em-nuvem. Acesso em: 9 nov. 2022.</em><small></small></p>
-                    <p className="lead fw-normal"><em>https://azure.microsoft.com/pt-pt/resources/cloud-computing-dictionary/what-is-iaas/</em></p>
-                    <p className="lead fw-normal"><em>https://azure.microsoft.com/pt-pt/resources/cloud-computing-dictionary/what-is-paas/</em></p>
-                    <p className="lead fw-normal"><em>https://azure.microsoft.com/pt-pt/resources/cloud-computing-dictionary/what-is-saas/</em></p>
-                    <p className="lead fw-normal"><em>Mahmood, Z. (2015). Cloud Computing: Methods and Practical Approaches (Computer Communications and Networks) (2013th ed.). Springer. p48.</em></p>
-                    <p className="lead fw-normal"><strong><em>Definição de Computação em Nuvem</em> : </strong><em>https://www.nist.gov/publications/nist-definition-cloud-computing</em><small></small></p>
-                    <p className="lead fw-normal"><em>https://www.gov.br/governodigital/pt-br/estrategias-e-politicas-digitais/computacao-em-nuvem/caracteristicas-essenciais-de-computacao-em-nuvem/</em></p>
-                    <p className="lead fw-normal"><strong><em>Glossário</em> : </strong><em>https://www.redhat.com/pt-br/topics/cloud-computing/what-are-cloud-providers</em><small></small></p>
-                    <p className="lead fw-normal"><em>https://docs.aws.amazon.com/pt_br/AmazonS3/latest/userguide/GetStartedWithS3.html</em></p>
-                    <p className="lead fw-normal"><em>https://hftecnologia.com.br/armazenamento-em-nuvem-como-funciona/</em></p>
+                    <p className="lead fw-normal"><em>O que é o IaaS? Infraestrutura como um Serviço | Microsoft Azure. Disponível em: https://azure.microsoft.com/pt-pt/resources/cloud-computing-dictionary/what-is-iaas/. Acesso em: 9 nov. 2022.</em></p>
+                    <p className="lead fw-normal"><em>O que é PaaS? Plataforma como Serviço | Microsoft Azure. Disponível em: https://azure.microsoft.com/pt-pt/resources/cloud-computing-dictionary/what-is-paas/. Acesso em: 9 nov. 2022.</em></p>
+                    <p className="lead fw-normal"><em>O que é SaaS? Software como Serviço | Microsoft Azure. Disponível em: https://azure.microsoft.com/pt-pt/resources/cloud-computing-dictionary/what-is-saas/. Acesso em: 9 nov. 2022.</em></p>
+                    <p className="lead fw-normal"><em>ZAIGHAM MAHMOOD. Cloud Computing Methods and Practical Approaches. [s.l.] London Springer, 2013. p. 48</em></p>
+                    <p className="lead fw-normal"><strong><em>Glossário</em>:</strong></p>
+                    <p className="lead fw-normal"><em>Conceitos básicos do Amazon S3 - Amazon Simple Storage Service. Disponível em: https://docs.aws.amazon.com/pt_br/AmazonS3/latest/userguide/GetStartedWithS3.html. Acesso em: 9 nov. 2022.</em></p>
+                    <p className="lead fw-normal"><em>O que são provedores de serviços em nuvem? Disponível em: https://www.redhat.com/pt-br/topics/cloud-computing/what-are-cloud-providers. Acesso em: 9 nov. 2022.</em><small></small></p>
+                    <p className="lead fw-normal"><em>TECNOLOGIA, H. Armazenamento em nuvem, como funciona? - HF Tecnologia. Disponível em: https://hftecnologia.com.br/armazenamento-em-nuvem-como-funciona/. Acesso em: 9 nov. 2022.</em></p>
                 </div>
             </section>
          
