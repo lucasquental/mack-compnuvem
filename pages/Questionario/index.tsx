d87e8bcac9da6350a86a4be46bdab7bd5478a78a
Lucas Quental <lucas.quental.001@gmail.com>
2022-11-11 22:54:39 -0300

    update

--- pages/Questionario/index.tsx
+++ pages/Questionario/index.tsx
@@ -4,10 +4,29 @@ import styles from './style.module.scss'
 export default function Questionario() {
 
     return (
-        <div>
+        <section className="mb-5"  style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '12px'}}>
+            <div>
+                <header className="py-5">
+                    <div className="container px-5">
+                        <div className="row justify-content-center">
+                            <div className="text-center">
+                                <h1 className="fw-bolder mb-3">Testes</h1>
+                                <p className="lead fw-normal text-muted">Selecione o teste que você deseja fazer.</p>
+                                <a type="button" className="btn btn-primary btn-lg  border border-dark" href="Questionario/modelo-servico">Modelos como serviço</a>
+                                <p></p>
+                                <a type="button" className="btn btn-primary btn-lg border border-dark" href="Questionario/conceitos-nuvem">Conceitos de Nuvem</a>
+                                <p></p>
+                                <a type="button" className="btn btn-primary btn-lg border border-dark" href="Questionario/quest-amazon">Questionário Amazon</a>
+                                <p></p>
+                                <a type="button" className="btn btn-primary btn-lg border border-dark" href="Questionario/quest-azure">Questionário Azure</a>
+                                <p></p>
+                                <a type="button" className="btn btn-primary btn-lg border border-dark" href="Questionario/quest-google">Questionário Google</a>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        </section>
 
-<iframe  style={{margin:'0 auto', justifyContent:'center'}} src="https://docs.google.com/forms/d/e/1FAIpQLSeGBo3PDYnxuV4QMJGOA1Zx2s5tJ31R7ci4NNAzNJJysDrSCQ/viewform?embedded=true"
-        width="640" height="2982" frameBorder="0"  >Carregando…</iframe>
-        </div>
     )
 }
--- pages/Questionario/modelo-servico/index.tsx
+++ pages/Questionario/modelo-servico/index.tsx
@@ -5,8 +5,7 @@ export default function Questionario() {
 
     return (
         <div className={styles.popbuttonQuest}>
-
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeGBo3PDYnxuV4QMJGOA1Zx2s5tJ31R7ci4NNAzNJJysDrSCQ/viewform?embedded=true" width="640" height="2982" frameBorder="0" >Carregando…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeGBo3PDYnxuV4QMJGOA1Zx2s5tJ31R7ci4NNAzNJJysDrSCQ/viewform?embedded=true" width="640" height="2982" frameBorder="0" >Carregando…</iframe>
         </div>
     )
 }
