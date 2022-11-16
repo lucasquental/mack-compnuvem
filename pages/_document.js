9c7c50a0536169dd512fb9530053123469058ff7
Lucas Quental <lucas.quental.001@gmail.com>
2022-11-16 14:20:10 -0300

    update

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript} from 'next/document'
+
+
+export default function Document() {
+    return (
+      <Html>
+        <Head>
+
+        <link rel="shortcut icon" href="Logo1.png" />
+     
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
